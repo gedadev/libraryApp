2d552129537caba3f5b7ad380b289c4ced2f041f
gedadev <gjbg1234@gmail.com>
2022-05-02 21:18:21 -0500

    Retrieved the data of the form and created a new object with the constructor which is pushed to the array

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let myLibrary = [];
+const form = document.querySelector('#form');
+const submit = document.querySelector('#submit');
+
 function Book (title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -8,5 +12,19 @@ function Book (title, author, pages, read) {
     }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
-console.log(theHobbit.info());
+// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
+//console.log(theHobbit.info());
+
+// submit.addEventListener('click', (e) => {
+//     e.preventDefault();
+// });
+
+form.addEventListener('submit', (e) => {
+    const data = Object.fromEntries(new FormData(e.target).entries());
+    addBookToLibrary(data);
+});
+
+function addBookToLibrary (data) {
+    myLibrary.push(new Book(data.title, data.author, data.pages, data.read));
+    console.log(myLibrary);
+}
