9acba9da6f9ea5d104d74b0ebc2d20733b63228c
gedadev <gjbg1234@gmail.com>
2022-06-06 22:37:58 -0500

    got the form element values with .elements and changed the submit button to a regular button to prevent the page to reload and reset the script

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 let myLibrary = [];
-const form = document.querySelector('#form');
-const submit = document.querySelector('#submit');
+const data = document.querySelector('#form').elements;  // HTMLFormElement.elements
+const btnSubmit = document.querySelector('#btnSubmit'); 
 
 function Book (title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function (){
-        return `${title} by ${author}, ${pages} pages, ${read}`;
-    }
+}
+
+Book.prototype.info = function(){
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 }
 
 // const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
-//console.log(theHobbit.info());
-
-// submit.addEventListener('click', (e) => {
-//     e.preventDefault();
-// });
-
-form.addEventListener('submit', (e) => {
-    const data = Object.fromEntries(new FormData(e.target).entries());
-    addBookToLibrary(data);
-});
+// console.log(theHobbit.info());
 
 function addBookToLibrary (data) {
-    myLibrary.push(new Book(data.title, data.author, data.pages, data.read));
+    myLibrary.push(new Book(data[0].value, data[1].value, data[2].value, data[3].value));
     console.log(myLibrary);
 }
+
+btnSubmit.addEventListener('click', () => {
+    addBookToLibrary(data);
+});
