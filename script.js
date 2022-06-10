5e88ef1745ab8343139333f08a1393cd8043b49a
gedadev <gjbg1234@gmail.com>
2022-06-09 21:37:17 -0500

    Added a function that displays the books info in a new div

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let myLibrary = [];
 const data = document.querySelector('#form').elements;  // HTMLFormElement.elements
 const btnSubmit = document.querySelector('#btnSubmit'); 
+const bookContainer = document.querySelector('#book-info');
 
 function Book (title, author, pages, read) {
     this.title = title;
@@ -19,8 +20,25 @@ Book.prototype.info = function(){
 function addBookToLibrary (data) {
     myLibrary.push(new Book(data[0].value, data[1].value, data[2].value, data[3].value));
     console.log(myLibrary);
+    displayBooks();
 }
 
 btnSubmit.addEventListener('click', () => {
     addBookToLibrary(data);
 });
+
+function displayBooks (){
+    for (let book in myLibrary) {
+        const bookCard = document.createElement('div');
+        bookCard.className = 'book-card';
+        const titleInfo = document.createTextNode(`Title: ${myLibrary[book].title}`);
+        bookCard.appendChild(titleInfo);
+        const authorInfo = document.createTextNode(`Author: ${myLibrary[book].author}`);
+        bookCard.appendChild(authorInfo);
+        const pagesInfo = document.createTextNode(`Pages: ${myLibrary[book].pages}`);
+        bookCard.appendChild(pagesInfo);
+        const readInfo = document.createTextNode(`Read?: ${myLibrary[book].read}`);
+        bookCard.appendChild(readInfo);
+        bookContainer.appendChild(bookCard);
+    }
+}
