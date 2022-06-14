be186f54616f0f56dac6e28865d1e5633953a7e9
gedadev <gjbg1234@gmail.com>
2022-06-13 21:10:28 -0500

    added a condition before appending the card to validate if the current id already exists in the dom tree

--- script.js
+++ script.js
@@ -20,27 +20,32 @@ Book.prototype.info = function(){
 
 function addBookToLibrary (data) {
     myLibrary.push(new Book(data[0].value, data[1].value, data[2].value, data[3].value));
-    displayBooks(Array.from(bookContainer.children));
 }
 
 btnSubmit.addEventListener('click', () => {
     addBookToLibrary(data);
+    displayBooks(Array.from(bookContainer.children));
 });
 
 function displayBooks(cardList){
-
     myLibrary.forEach((element, index) => {
         const bookCard = document.createElement('div');
         bookCard.classList.add('book-card');
         bookCard.id = `book-${index}`;
-        bookContainer.appendChild(bookCard);
-        for(let value in Object.entries(element)){
-            let keys = Object.keys(element);
-            let bookInfo = document.createElement('p');
-            let val = document.createTextNode(`${keys[value]}: ${element[keys[value]]}`);
-            bookInfo.appendChild(val);
-            bookCard.appendChild(bookInfo);
-            bookInfo.className = 'book-info';
+        
+        // validate if the id already exists in the children of the bookContainer
+        try {
+            if (!(cardList[index].id === bookCard.id)) throw undefined;
+        } catch (e) {
+            bookContainer.appendChild(bookCard);
+            for(let value in Object.entries(element)){
+                let keys = Object.keys(element);
+                let bookInfo = document.createElement('p');
+                let val = document.createTextNode(`${keys[value]}: ${element[keys[value]]}`);
+                bookInfo.appendChild(val);
+                bookCard.appendChild(bookInfo);
+                bookInfo.className = 'book-info';
+            }
         }
     });
 }
